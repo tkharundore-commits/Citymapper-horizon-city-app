a051a9e8abbdf99e915adf75a1a51aea3c57f66b
tkharundore-commits <taherkh.arundore@gmail.com>
2026-02-05 10:56:23 +0100

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function moveCarousel(gridId, direction) {
-    const grid = document.getElementById(gridId);
-    const scrollAmount = 320; // Largeur d'une carte + gap
-    grid.scrollBy({
-        left: direction * scrollAmount,
-        behavior: 'smooth'
-    });
-}
